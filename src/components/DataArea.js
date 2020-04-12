68dc806ec8cc50451e6ad823f28c01fd6fd690d7
Tucker Vassau <tuckv@hotmail.com>
2020-04-11 19:18:36 -0600

    Setup Axios for pulling random data

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Nav from './Nav';
 import '../style/DataArea.css';
+import API from "../utils/API";
 
 export default function DataArea() {
+
+    useEffect(() => {
+        API.getUsers().then(results => {
+          console.log(results.data.results);
+        //   setDeveloperState({
+        //     ...developerState,
+        //     users: results.data.results,
+        //     filteredUsers: results.data.results
+        //   });
+        });
+      }, []);
+
     return (
         <div className="dataArea">
             <Nav />

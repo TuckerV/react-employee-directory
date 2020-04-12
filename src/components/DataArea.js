1d6e6324f63a21fce8b37ffa352990dbbbaf1854
Tucker Vassau <tuckv@hotmail.com>
2020-04-11 19:26:38 -0600

    Setup context and Provider

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -1,9 +1,24 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Nav from './Nav';
 import '../style/DataArea.css';
 import API from "../utils/API";
+import DataAreaContext from "../utils/DataAreaContext";
 
 export default function DataArea() {
+    const [developerState, setDeveloperState] = useState({
+        users: [],
+        order: "descend",
+        filteredUsers: [],
+        headings: [
+          { name: "Image", width: "10%", order: "descend" },
+          { name: "name", width: "10%", order: "descend" },
+          { name: "phone", width: "20%", order: "descend" },
+          { name: "email", width: "20%", order: "descend" },
+          { name: "dob", width: "10%", order: "descend" }
+        ]
+      });
+
+      
 
     useEffect(() => {
         API.getUsers().then(results => {
@@ -17,8 +32,10 @@ export default function DataArea() {
       }, []);
 
     return (
-        <div className="dataArea">
-            <Nav />
-        </div>
+        <DataAreaContext.Provider>
+            <div className="dataArea">
+                <Nav />
+            </div>
+        </DataAreaContext.Provider>
     )
 }

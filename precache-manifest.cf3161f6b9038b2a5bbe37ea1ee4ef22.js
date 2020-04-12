5d814a4565e60a8cecd439817081683094a26d9c
Tucker Vassau <tuckv@hotmail.com>
2020-04-11 21:29:05 -0600

    Updates

---
+++ precache-manifest.cf3161f6b9038b2a5bbe37ea1ee4ef22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e29a0e9e77acf66334eccd38f24d461d",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "195c6d56c4a09af90c71",
+    "url": "/react-employee-directory/static/css/main.c3ea7092.chunk.css"
+  },
+  {
+    "revision": "6e4529432c9dcf4a7b15",
+    "url": "/react-employee-directory/static/js/2.0ea13ed6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.0ea13ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "195c6d56c4a09af90c71",
+    "url": "/react-employee-directory/static/js/main.8436ef53.chunk.js"
+  },
+  {
+    "revision": "9f229d4e0543080e8923",
+    "url": "/react-employee-directory/static/js/runtime-main.57c7f57a.js"
+  }
+]);

984027ded99af8ad418844a8afeac6c2f6790058
Tucker Vassau <tuckv@hotmail.com>
2020-04-11 18:44:27 -0600

    Wrapper and header laid out

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../style/Header.css";
+
+export default function Header() {
+    return (
+        <div>
+            <div className="header">
+                <h1>Employee Directory</h1>
+            </div>            
+        </div>
+    )
+}
